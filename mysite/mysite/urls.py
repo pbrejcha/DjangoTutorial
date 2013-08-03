1c28783f78f6986cc8c380289236458a15b098a6
pbrejcha <peter.brejcha@gmail.com>
2013-08-03 16:15:05 -0500

    finished part 3

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,6 +15,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     # ex: /polls/
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
 
 )
