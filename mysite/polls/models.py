2c089e4ba498c6678ee12ca833d5a6e32778e9ec
pb <peter.brejcha@slalom.com>
2013-08-09 14:50:08 -0500

    test changes

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 class Poll(models.Model):
 	question = models.CharField(max_length = 200)
-	pub_date = models.DateTimeField('date published')
+	pub_date = models.DateTimeField('Date Published')
 
 	def __unicode__(self):
 		return self.question
